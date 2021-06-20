e560b8befbc99405f029bc52b155eb8d4a92c33d
Mikita Kandratsyeu <nickondr.production@gmail.com>
2021-06-20 22:51:50 +0300

    Added API end points

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import tabReducer from './tab/tab.reducer';
+import marketReducer from './market/market.reducer';
 
 export default combineReducers({
   tabReducer,
+  marketReducer,
 });
